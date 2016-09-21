d8d74cea84e0c62ade6e1f3253c4199513f8c707
annsim <anna.simonsson@comhem.com>
2016-09-21 15:18:53 +0200

    Change-Id: I259d68a4b91df5bc7781353dc3827ddac3db3d68

--- BankAccount2/src/Customer.java
+++ BankAccount2/src/Customer.java
@@ -4,19 +4,22 @@ public class Customer extends SavingsAccount {
 	private String firstName;
 	private String lastName;
 	private long persNr;
+	
+	private SavingsAccount account = new SavingsAccount();
 
-	Customer( String firstName,String lastName, long persNr){
+	Customer( String firstName,String lastName, long persNr,SavingsAccount account ){
 	
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.persNr = persNr;
-	
+	this.account = account;
 }
 	public String toString(){
 		return
 				"Förnamn: "+firstName+"\n"+
 				"Efternamn: "+lastName+"\n"+
-				"Person nummer: "+persNr+"\n";
+				"Person nummer: "+persNr+"\n"+
+				"Konto nummer: "+account+"\n";
 				
 		
 	}
